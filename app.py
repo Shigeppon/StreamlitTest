b2dc9c5b50ad75932575d7331ba537aef9a575e7
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2024-08-06 12:02:32 +0900

    format

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ st.image("https://ul.h3z.jp/tbfgZLSX.webp")
 
 # テーブル
 import pandas as pd
+
 df = pd.DataFrame(
-        {
-            "first column": [1, 2, 3, 4],
-            "second column": [10, 20, 30, 40],
-        }
-    )
+    {
+        "first column": [1, 2, 3, 4],
+        "second column": [10, 20, 30, 40],
+    }
+)
 
 st.write(df)
 
@@ -31,7 +32,7 @@ st.line_chart(df)
 name = st.text_input("名前")
 
 # 数値入力
-age = st.number_input("年齢",step=1)
+age = st.number_input("年齢", step=1)
 
 st.write(f"名前: {name}")
 st.write(f"年齢: {age}")
@@ -41,11 +42,11 @@ if st.button("Push"):
     st.write("ボタンを押しました")
 
 # プルダウン
-select = st.selectbox("好きな果物",options=["りんご","ばなな","いちご"])
+select = st.selectbox("好きな果物", options=["りんご", "ばなな", "いちご"])
 st.write(select)
 
 # プルダウン(複数選択)
-multi_select = st.multiselect("好きな色",options=["赤","青","黄"])
+multi_select = st.multiselect("好きな色", options=["赤", "青", "黄"])
 st.write(multi_select)
 
 # チェックボックス
@@ -57,7 +58,7 @@ radio = st.radio("選択", ["猫", "犬"])
 st.write(f"ラジオ：{radio}")
 
 # ファイルアップロード
-uploaded_file=st.file_uploader("Upload",type=["csv"])
+uploaded_file = st.file_uploader("Upload", type=["csv"])
 if uploaded_file:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
@@ -74,12 +75,12 @@ with cols[1]:
     st.write("列2")
 
 # タブ
-tabs = st.tabs(["タブ1","タブ2"])
+tabs = st.tabs(["タブ1", "タブ2"])
 with tabs[0]:
     st.write("タブ1")
 with tabs[1]:
     st.write("タブ2")
-    
+
 # アコーディオン
 with st.expander("開きます"):
     st.write("開きました")
