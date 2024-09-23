c55b71f475cf70d80808b98c6415b304c428a305
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2024-09-23 11:33:53 +0900

    add 5-3-4.py

---
+++ app5-3-4.py
@@ -0,0 +1,156 @@
+import traceback
+import streamlit as st
+import tiktoken
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+# models
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from urllib.parse import urlparse
+
+from langchain_community.document_loaders import YoutubeLoader
+
+from langchain_core.runnables import RunnableLambda
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+SUMMARIZE_PROMPT = """以下のコンテンツについて、内容を300文字程度でわかりやすく要約してください。
+
+========
+
+{content}
+
+========
+
+日本語で書いてね！
+"""
+
+
+def init_page():
+    st.set_page_config(page_title="Website Summarizer")
+    st.header("Website Summarizer")
+    st.sidebar.title("Options")
+
+
+def select_model(temperature=0):
+    models = ("GPT-3.5", "GPT-4", "Claude 3.5 Sonnet", "Gemini 1.5 Pro")
+    model = st.sidebar.radio("Choose a model:", models)
+    if model == "GPT-3.5":
+        st.session_state.model_name = "gpt-3.5-turbo"
+        return ChatOpenAI(
+            temperature=temperature, model_name=st.session_state.model_name
+        )
+    elif model == "GPT-4":
+        st.session_state.model_name = "gpt-4o"
+        return ChatOpenAI(
+            temperature=temperature, model_name=st.session_state.model_name
+        )
+    elif model == "Claude 3.5 Sonnet":
+        st.session_state.model_name = "claude-3-5-sonnet-20240620"
+        return ChatAnthropic(
+            temperature=temperature, model_name=st.session_state.model_name
+        )
+    elif model == "Gemini 1.5 Pro":
+        st.session_state.model_name = "gemini-1.5-pro-latest"
+        return ChatGoogleGenerativeAI(
+            temperature=temperature, model=st.session_state.model_name
+        )
+
+
+def init_summarize_chain():
+    llm = select_model()
+    prompot = ChatPromptTemplate.from_messages([("user", SUMMARIZE_PROMPT)])
+    output_parser = StrOutputParser()
+    return prompot | llm | output_parser
+
+
+def init_chain():
+    summarize_chain = init_summarize_chain()
+
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        model_name="gpt-3.5-turbo", chunk_size=16000, chunk_overlap=0
+    )
+
+    text_split = RunnableLambda(
+        lambda x: [{"content": doc} for doc in text_splitter.split_text(x["content"])]
+    )
+    text_concat = RunnableLambda(lambda x: {"content": "\n".join(x)})
+    map_reduce_chain = (
+        text_split | summarize_chain.map() | text_concat | summarize_chain
+    )
+
+    def route(x):
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        token_count = len(encoding.encode(x["content"]))
+        if token_count > 16000:
+            return map_reduce_chain
+        else:
+            return summarize_chain
+
+    chain = RunnableLambda(route)
+    return chain
+
+
+def validate_url(url):
+    """URLが有効かどうかを判定する関数"""
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_content(url):
+    """
+    Document:
+        - page_content: str
+        - metadata: dict
+
+            - source: str
+            - title: str
+            - description: Optional[str]
+            - view_count: int
+            - thumbnail_url: Optional[str]
+            - publish_date: str
+            - length: int
+            - author: str
+    """
+    with st.spinner("Fetching Website ..."):
+        loader = YoutubeLoader.from_youtube_url(
+            url, add_video_info=True, language=["en", "ja"]
+        )
+        res = loader.load()
+
+        try:
+            if res:
+                content = res[0].page_content
+                title = res[0].metadata["title"]
+                return f"Title: {title}\n\n{content}"
+            else:
+                return None
+        except:
+            st.write(traceback.format_exc())
+            return None
+
+
+def main():
+    init_page()
+    chain = init_chain()
+
+    if url := st.chat_input("URL: ", key="input"):
+        is_valid_url = validate_url(url)
+        if not is_valid_url:
+            st.write("Please input valid url")
+        else:
+            if content := get_content(url):
+                st.markdown("## Summary")
+                st.write_stream(chain.stream({"content": content}))
+                st.markdown("---")
+                st.markdown("## Original Text")
+                st.write(content)
+
+
+if __name__ == "__main__":
+    main()
